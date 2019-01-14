5b32f29b54f2248f12dcb57c6cd9a41a5487d211
marcel-k <marcel.van.de.kamp@ordina.nl>
2019-01-14 16:36:22 +0100

    stateless register button and list component

---
+++ src/components/button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.css';
+
+function Button() {
+  return (
+    <button className={'button button-register'}>Register</button>
+  );
+}
+
+export default Button;
