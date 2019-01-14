7afac1c81ca1fec277d9815fea68f4fa04f78722
marcel-k <marcel.van.de.kamp@ordina.nl>
2019-01-14 07:14:28 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+/**
+ * This assignment is just getting used to jsx and creating the layout of the app:
+* - assigment:
+*   - use the render lifecycle function and JSX in the class component App to:
+*   - create the layout of the app in the app component.
+*     - use css grid: header, footer, menu, main.
+*     - give al elements a min-height and a border. 
+* Now we defined that we have defined our layout like this, 
+* we actually defined our second layer of components in our component tree, app being the first layer.
+*/
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className={'container'}>
+        <header className={'header'}>
         </header>
+        <div className={'menu'}>
+        </div>
+        <main className={'main'}>
+        </main>
+        <footer className={'footer'}>
+        </footer>
       </div>
     );
   }
