e42b20734bfab91066d6d9fe34c59daac6fda13a
marcel-k <marcel.van.de.kamp@ordina.nl>
2019-01-14 16:54:10 +0100

    bug: use list component

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,41 +1,11 @@
 import React from 'react';
+import List from '../list/List';
 import './Menu.css';
 
 function Menu() {
   return (
     <div className={'menu'}>
-      <ul className={'list'}>
-        <li className={'list-item'}>
-          <button className={'wrapper'}>
-            <span className={'text'}>React Deep Dive I</span>
-            <span className={'subtext'}>22 feb 2019</span>
-          </button>
-        </li>
-        <li className={'list-item'}>
-          <button className={'wrapper'}>
-            <span className={'text'}>Angular Deep Dive I</span>
-            <span className={'subtext'}>8 feb 2019</span>
-          </button>
-        </li>
-        <li className={'list-item'}>
-          <button className={'wrapper'}>
-            <span className={'text'}>React Introduction</span>
-            <span className={'subtext'}>25 jan 2019</span>
-          </button>
-        </li>
-        <li className={'list-item'}>
-          <button className={'wrapper'}>
-            <span className={'text'}>Angular Introduction</span>
-            <span className={'subtext'}>11 jan 2019</span>
-          </button>
-        </li>
-        <li className={'list-item'}>
-          <button className={'wrapper'}>
-            <span className={'text'}>CSS Grid & Flexbox</span>
-            <span className={'subtext'}>11 jan 2019</span>
-          </button>
-        </li>
-      </ul>
+     <List />
     </div>
   );
 }
