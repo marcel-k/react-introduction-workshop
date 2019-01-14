4d521e2f3442c9d54cf5369f10566e95d608e903
marcel-k <marcel.van.de.kamp@ordina.nl>
2019-01-14 16:31:36 +0100

    class rename

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -6,33 +6,33 @@ function Menu() {
     <div className={'menu'}>
       <ul className={'list'}>
         <li className={'list-item'}>
-          <button className={'link'}>
-            <span className={'title'}>React Deep Dive I</span>
-            <span className={'date'}>22 feb 2019</span>
+          <button className={'wrapper'}>
+            <span className={'text'}>React Deep Dive I</span>
+            <span className={'subtext'}>22 feb 2019</span>
           </button>
         </li>
         <li className={'list-item'}>
-          <button className={'link'}>
-            <span className={'title'}>Angular Deep Dive I</span>
-            <span className={'date'}>8 feb 2019</span>
+          <button className={'wrapper'}>
+            <span className={'text'}>Angular Deep Dive I</span>
+            <span className={'subtext'}>8 feb 2019</span>
           </button>
         </li>
         <li className={'list-item'}>
-          <button className={'link'}>
-            <span className={'title'}>React Introduction</span>
-            <span className={'date'}>25 jan 2019</span>
+          <button className={'wrapper'}>
+            <span className={'text'}>React Introduction</span>
+            <span className={'subtext'}>25 jan 2019</span>
           </button>
         </li>
         <li className={'list-item'}>
-          <button className={'link'}>
-            <span className={'title'}>Angular Introduction</span>
-            <span className={'date'}>11 jan 2019</span>
+          <button className={'wrapper'}>
+            <span className={'text'}>Angular Introduction</span>
+            <span className={'subtext'}>11 jan 2019</span>
           </button>
         </li>
         <li className={'list-item'}>
-          <button className={'link'}>
-            <span className={'title'}>CSS Grid & Flexbox</span>
-            <span className={'date'}>11 jan 2019</span>
+          <button className={'wrapper'}>
+            <span className={'text'}>CSS Grid & Flexbox</span>
+            <span className={'subtext'}>11 jan 2019</span>
           </button>
         </li>
       </ul>
