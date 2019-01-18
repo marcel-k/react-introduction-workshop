2973c88da5ba75b62cea76ead1d649e3f6df569b
marcel-k <marcel.van.de.kamp@ordina.nl>
2019-01-18 17:44:03 +0100

    container component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import Menu from './components/menu/Menu';
-import Content from './components/content/Content';
+import MenuContentContainer from './components/menu-content-container/MenuContentContainer';
 import './App.css';
-
-const title = 'React Introduction';
-
-const paragraphs = [
-  `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-  `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
-]
 
 class App extends Component {
 
@@ -18,8 +10,7 @@ class App extends Component {
     return (
       <div className={'container'}>
         <Header />
-        <Menu />
-        <Content title={title} paragraphs={paragraphs} />
+        <MenuContentContainer />
         <Footer />
       </div>
     );
