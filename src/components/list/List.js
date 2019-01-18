59958e43843308ef1740e4c7e9f7c807568579be
marcel-k <marcel.van.de.kamp@ordina.nl>
2019-01-18 10:43:28 +0100

    array toch alvast verplaatsen..

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -2,35 +2,9 @@ import React from 'react';
 import ListItem from './ListItem';
 import './List.css';
 
-const items = [
-  {
-    id: 1,
-    text: 'React Deep Dive I',
-    subText: '22 feb 2019'
-  },
-  {
-    id: 2,
-    text: 'Angular Deep Dive I',
-    subText: '8 feb 2019'
-  },
-  {
-    id: 3,
-    text: 'React Introduction',
-    subText: '25 jan 2019'
-  },
-  {
-    id: 4,
-    text: 'Angular Introduction',
-    subText: '11 jan 2019'
-  },
-  {
-    id: 5,
-    text: 'CSS Grid & Flexbox',
-    subText: '11 jan 2019'
-  }
-];
+function List(props) {
+  const { items } = props;
 
-function List() {
   return (
     <ul className={'list'}>
       {items.map((item) => (
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -2,10 +2,38 @@ import React from 'react';
 import List from '../list/List';
 import './Menu.css';
 
+const menuItems = [
+  {
+    id: 1,
+    text: 'React Deep Dive I',
+    subText: '22 feb 2019'
+  },
+  {
+    id: 2,
+    text: 'Angular Deep Dive I',
+    subText: '8 feb 2019'
+  },
+  {
+    id: 3,
+    text: 'React Introduction',
+    subText: '25 jan 2019'
+  },
+  {
+    id: 4,
+    text: 'Angular Introduction',
+    subText: '11 jan 2019'
+  },
+  {
+    id: 5,
+    text: 'CSS Grid & Flexbox',
+    subText: '11 jan 2019'
+  }
+];
+
 function Menu() {
   return (
     <div className={'menu'}>
-      <List />
+      <List items={menuItems} />
     </div>
   );
 }
