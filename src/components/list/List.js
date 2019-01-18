277b94b4a3395edbd17ec04a45a64972242368d9
marcel-k <marcel.van.de.kamp@ordina.nl>
2019-01-18 10:25:35 +0100

    move array to list first

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -2,9 +2,35 @@ import React from 'react';
 import ListItem from './ListItem';
 import './List.css';
 
-function List(props) {
-  const { items } = props;
+const items = [
+  {
+    id: 1,
+    text: 'React Deep Dive I',
+    subText: '22 feb 2019'
+  },
+  {
+    id: 2,
+    text: 'Angular Deep Dive I',
+    subText: '8 feb 2019'
+  },
+  {
+    id: 3,
+    text: 'React Introduction',
+    subText: '25 jan 2019'
+  },
+  {
+    id: 4,
+    text: 'Angular Introduction',
+    subText: '11 jan 2019'
+  },
+  {
+    id: 5,
+    text: 'CSS Grid & Flexbox',
+    subText: '11 jan 2019'
+  }
+];
 
+function List() {
   return (
     <ul className={'list'}>
       {items.map((item) => (
