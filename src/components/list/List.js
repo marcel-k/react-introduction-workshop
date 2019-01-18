cd681789a7e131f93edd4a7c4bbf7fb355c72d5a
marcel-k <marcel.van.de.kamp@ordina.nl>
2019-01-18 10:14:35 +0100

    hardcoded listitem

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,40 +1,31 @@
 import React from 'react';
+import ListItem from './ListItem';
 import './List.css';
 
 function List() {
   return (
-      <ul className={'list'}>
-        <li className={'list-item'}>
-          <button className={'wrapper'}>
-            <span className={'text'}>React Deep Dive I</span>
-            <span className={'subtext'}>22 feb 2019</span>
-          </button>
-        </li>
-        <li className={'list-item'}>
-          <button className={'wrapper'}>
-            <span className={'text'}>Angular Deep Dive I</span>
-            <span className={'subtext'}>8 feb 2019</span>
-          </button>
-        </li>
-        <li className={'list-item'}>
-          <button className={'wrapper'}>
-            <span className={'text'}>React Introduction</span>
-            <span className={'subtext'}>25 jan 2019</span>
-          </button>
-        </li>
-        <li className={'list-item'}>
-          <button className={'wrapper'}>
-            <span className={'text'}>Angular Introduction</span>
-            <span className={'subtext'}>11 jan 2019</span>
-          </button>
-        </li>
-        <li className={'list-item'}>
-          <button className={'wrapper'}>
-            <span className={'text'}>CSS Grid & Flexbox</span>
-            <span className={'subtext'}>11 jan 2019</span>
-          </button>
-        </li>
-      </ul>
+    <ul className={'list'}>
+        <ListItem
+          text={'React Deep Dive I'}
+          subText={'22 feb 2019'}
+        />
+        <ListItem
+          text={'Angular Deep Dive I'}
+          subText={'8 feb 2019'}
+        />
+        <ListItem
+          text={'React Introduction'}
+          subText={'25 jan 2019'}
+        />
+        <ListItem
+          text={'Angular Introduction'}
+          subText={'11 jan 2019'}
+        />
+        <ListItem
+          text={'CSS Grid & Flexbox'}
+          subText={'11 jan 2019'}
+        />
+    </ul>
   );
 }
 
