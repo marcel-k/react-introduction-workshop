a9e99930e9fde7cbcb39c9d3beb06bb25633de25
marcel-k <marcel.van.de.kamp@ordina.nl>
2019-01-19 16:20:42 +0100

    fetch meetups via componentdidmount

--- src/components/menu-content-container/MenuContentContainer.js
+++ src/components/menu-content-container/MenuContentContainer.js
@@ -2,55 +2,6 @@ import React from 'react';
 import Menu from '../menu/Menu';
 import Content from '../content/Content';
 
-/** 
- * Data from the back-end
- */
-const menuItems = [
-  {
-    id: 1,
-    title: 'React Deep Dive I',
-    date: '22 feb 2019'
-  },
-  {
-    id: 2,
-    title: 'Angular Deep Dive I',
-    date: '8 feb 2019'
-  },
-  {
-    id: 3,
-    title: 'React Introduction',
-    date: '25 jan 2019',
-    current: true,
-    registered: true
-  },
-  {
-    id: 4,
-    title: 'Angular Introduction',
-    date: '11 jan 2019',
-    registered: true
-  },
-  {
-    id: 5,
-    title: 'Fresh up your CSS Skills',
-    date: '30 november 2018'
-  },
-  {
-    id: 6,
-    title: 'HTML semantics and accesibility',
-    date: '2 november 2018'
-  },
-  {
-    id: 7,
-    title: 'JavaScript and TypeScript Fundamentals',
-    date: '19 october 2018'
-  },
-  {
-    id: 8,
-    title: 'Human Centered Design',
-    date: '21 september 2018'
-  }
-];
-
 const paragraphs = [
   `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
   `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
@@ -63,8 +14,8 @@ class MenuContentContainer extends React.Component {
     super(props);
 
     this.state = {
-      selectedItemId: 1,
-      items: menuItems // should do this in componentDidMount
+      selectedItemId: 0,
+      items: []
     };
   }
 
@@ -92,13 +43,18 @@ class MenuContentContainer extends React.Component {
   }
 
   async componentDidMount() {
-    // const response = await fetch(apiUrl + '/meetups');
-    // const menuItems = await response.json();
+    const response = await fetch(apiUrl + '/meetups');
+    const menuItems = await response.json();
 
+    this.setState({
+      selectedItemId: menuItems[0].id,
+      items: menuItems
+    });
   }
 
   render() {
-    const selectedItem = this.state.items.find((item) => item.id === this.state.selectedItemId);
+    let selectedItem = this.state.items.find((item) => item.id === this.state.selectedItemId);
+    selectedItem = selectedItem || { };
 
     return (
       <>
