fde1c28cbd6695334ab970171310e9d776b45a7f
marcel-k <marcel.van.de.kamp@ordina.nl>
2019-01-19 10:57:13 +0100

    meetups uitbreiden

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -34,8 +34,26 @@ function Menu() {
         </li>
         <li className={'list-item'}>
           <button className={'wrapper'}>
-            <span className={'text'}>CSS Grid & Flexbox</span>
-            <span className={'subtext'}>11 jan 2019</span>
+            <span className={'text'}>Fresh up your CSS Skills</span>
+            <span className={'subtext'}>30 november 2018</span>
+          </button>
+        </li>
+        <li className={'list-item'}>
+          <button className={'wrapper'}>
+            <span className={'text'}>HTML semantics and accesibility</span>
+            <span className={'subtext'}>2 november 2018</span>
+          </button>
+        </li>
+        <li className={'list-item'}>
+          <button className={'wrapper'}>
+            <span className={'text'}>JavaScript and TypeScript Fundamentals</span>
+            <span className={'subtext'}>19 october 2018</span>
+          </button>
+        </li>
+        <li className={'list-item'}>
+          <button className={'wrapper'}>
+            <span className={'text'}>Human Centered Design</span>
+            <span className={'subtext'}>21 september 2018</span>
           </button>
         </li>
       </ul>
