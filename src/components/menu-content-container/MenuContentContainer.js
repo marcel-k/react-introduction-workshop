bdf7e04cdfa4c6a04ae092fc64efbc0f662b46ac
marcel-k <marcel.van.de.kamp@ordina.nl>
2019-01-19 12:08:26 +0100

    title from selected item

--- src/components/menu-content-container/MenuContentContainer.js
+++ src/components/menu-content-container/MenuContentContainer.js
@@ -52,8 +52,6 @@ const menuItems = [
   }
 ];
 
-const title = 'React Introduction';
-
 const paragraphs = [
   `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
   `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
@@ -68,7 +66,7 @@ class MenuContentContainer extends React.Component {
       <>
         <Menu menuItems={menuItems} />
         <Content
-          title={title}
+          title={selectedItem.title}
           paragraphs={paragraphs}
           registered={selectedItem.registered}
         />
