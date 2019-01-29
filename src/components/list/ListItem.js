e5c24a3130a215d143e231ad38f409bb379a6c0e
marcel-k <mailme@marcel-k.net>
2019-01-29 09:39:23 +0100

    geen ul

--- src/components/list/ListItem.js
+++ src/components/list/ListItem.js
@@ -4,14 +4,12 @@ import './ListItem.css';
 function ListItem(props) {
   const { text, subText } = props;
   return (
-    <ul className={'list'}>
-      <li className={'list-item'}>
-        <button className={'wrapper'}>
-          <span className={'text'}>{text}</span>
-          <span className={'subtext'}>{subText}</span>
-        </button>
-      </li>
-    </ul>
+    <li className={'list-item'}>
+      <button className={'wrapper'}>
+        <span className={'text'}>{text}</span>
+        <span className={'subtext'}>{subText}</span>
+      </button>
+    </li>
   );
 }
 
