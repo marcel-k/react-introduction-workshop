e75c447e71a83d1956d7cb249191d8761ca36c27
marcel-k <marcel.van.de.kamp@ordina.nl>
2019-07-05 15:31:41 +0200

    convert to hooks

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,67 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import List from '../list/List';
 import Search from '../search/Search';
 import ListItem from '../list/ListItem';
 import { classNames } from '../../utils/utils';
 import './Menu.css';
 
-class Menu extends React.Component {
 
-  constructor(props) {
-    super(props);
+function filterMenuItems(menuItems, searchText) {
+  const filteredItems =
+    menuItems.filter((item) => {
+      const itemText = item.title.toLowerCase();
+      const lowerCase = searchText.toLowerCase();
 
-    this.state = { searchText: '' };
-  }
+      if (itemText.indexOf(lowerCase) !== -1) {
+        return true;
+      }
 
-  filterMenuItems() {
-    const filteredItems =
-      this.props.menuItems.filter((item) => {
-        const itemText = item.title.toLowerCase();
-        const searchText = this.state.searchText.toLowerCase();
-
-        if (itemText.indexOf(searchText) !== -1) {
-          return true;
-        }
-
-        return false;
-      });
-
-    return filteredItems;
-  }
-
-  handleInputChange(text) {
-    this.setState({
-      searchText: text
+      return false;
     });
-  }
 
-  render() {
-    const { selectedItemId, onMenuItemClick } = this.props;
-    const filteredItems = this.filterMenuItems();
+  return filteredItems;
+}
 
-    return (
-      <div className={'menu'}>
-        <Search 
-        searchText={this.state.searchText}
-        onInputChange={(value) => this.handleInputChange(value)} />
-        <List>
-          {filteredItems.map((item) => (
-            <ListItem
-              key={item.id}
-              text={item.title}
-              subText={item.date}
-              onClick={() => onMenuItemClick(item.id)}
-              cssClasses={classNames({
-                'current': item.current,
-                'registered': item.registered,
-                'selected': item.id === selectedItemId
-              })}
-            />)
-          )}
-        </List>
-      </div>
-    );
-  }
+function Menu (props) {
+  const {
+    menuItems,
+    selectedItemId,
+    onMenuItemClick
+  } = props;
+  const [searchText, setSearchText] = useState('');
+
+  const filteredItems = filterMenuItems(menuItems, searchText);
+
+  return (
+    <div className={'menu'}>
+      <Search
+        searchText={searchText}
+        onInputChange={setSearchText} />
+      <List>
+        {filteredItems.map((item) => (
+          <ListItem
+            key={item.id}
+            text={item.title}
+            subText={item.date}
+            onClick={() => onMenuItemClick(item.id)}
+            cssClasses={classNames({
+              'current': item.current,
+              'registered': item.registered,
+              'selected': item.id === selectedItemId
+            })}
+          />)
+        )}
+      </List>
+    </div>
+  );
 }
 
 export default Menu;
